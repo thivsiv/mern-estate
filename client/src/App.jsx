6c965fcde1edfb822b8e74cf98ed3db47a2e1ea4
thivi <thivi_email@example.com>
2024-09-23 19:12:56 +0530

    create server and run

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+export default function App() {
+  return <BrowserRouter> 
+  <Header />
+  <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/sign-in" element={<SignIn />} />
+  <Route path="/sign-up" element={<SignUp />} />
+  <Route path="/about" element={<About />} />
+  <Route path="/profile" element={<Profile />} />
+  </Routes>
+  </BrowserRouter>;
+  
+}
